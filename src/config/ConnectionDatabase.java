6356b5569f1b765510d12752cb967bf7c85d91b8
Tengku rafi <t.mrr39@gmail.com>
2022-11-14 13:54:56 +0700

    first commit

---
+++ src/config/ConnectionDatabase.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**s
+ *
+ * @author BISMILLAH
+ */
+public class ConnectionDatabase {
+    static String connectionUrl = "jdbc:sqlserver://LAPTOP-72DGJ6K6:1433;databaseName=Gojek;user=sa;password=12345678;encrypt=false;trustServerCertificate=true;";
+    static Connection con;
+    
+    public static Connection getConnection () {
+        try {
+            con = DriverManager.getConnection(connectionUrl); 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
+
